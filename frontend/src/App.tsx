88d27feffbfa3b946043f7e9263f14fb778df127
Man Kam Hung <kamhung@vivovii.com>
2018-07-20 17:15:32 +0800

    boostrapping with frontend React and backend nodeJS using indeed API

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,10 +3,11 @@ import './App.css';
 
 // import ResumeInput from './scenes/resumeInput'
 
+
 import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
+import MenuIcon from '@material-ui/core/Icon';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/core/icon';
 import Toolbar from '@material-ui/core/Toolbar';
 // import Typography from '@material-ui/core/Typography';
 // import MenuIcon from '@material-ui/icons/Menu';
